1dc971ac32bec9222fdfc6897803bfd661ca8cfa
Kay <92918293+AgualongoK@users.noreply.github.com>
2023-02-08 22:04:37 +0100

    update

--- T1_Inicio/app/src/main/java/com/example/t1_inicio/MainActivity.kt
+++ T1_Inicio/app/src/main/java/com/example/t1_inicio/MainActivity.kt
@@ -2,10 +2,65 @@ package com.example.t1_inicio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var botonPulsar: Button
+    lateinit var textoSaludo: TextView
+    lateinit var campoTexto : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // xml + kt
+        // Log.v("ciclo_vida","Método onCreate ejecutando")
+        // cuando pulse el boton que ponga un texto en el textview
+        campoTexto = findViewById(R.id.mensaje)
+        botonPulsar =  findViewById(R.id.boton_pulsar)
+        textoSaludo = findViewById(R.id.texto_bienvenida)
+
+        botonPulsar.setOnClickListener({
+            /*// textoSaludo.setText("Primera app completa")
+            textoSaludo.setText(campoTexto.text)
+            campoTexto.setText("")
+            Log.v("ciclo_vida","Boton pulsado")*/
+
+            if (!campoTexto.text.isEmpty()){
+                textoSaludo.setText(campoTexto.text)
+                campoTexto.setText("")
+            }else{
+                Log.v("avisos"," texto introducido vacío")
+            }
+        })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.v("ciclo_vida","Metodo onStart ejecutado")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v("ciclo_vida","Metodo onResume ejecutado")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v("ciclo_vida","Metodo onPause ejecutado")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v("ciclo_vida","Metodo onPause ejecutado")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v("ciclo_vida","Metodo onDestroy ejecutado")
     }
 }
